9260fd148e3280066c175a21772131acdf69c9b6
Lior Belinsky <liorbe@wix.com>
2017-12-02 01:47:31 +0200

    use only explicit --path

--- import-path.js
+++ import-path.js
@@ -4,7 +4,7 @@ const program = require('commander');
 
 program
 .version('0.0.1')
-.option('-p, --path [path]', 'Path inside the package to improve')
+.option('--path [path]', 'Path inside the package to improve')
 .parse(process.argv);
 
 require('./src/scan')(program.path);
